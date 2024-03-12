a8143fea9f8b17ff2d23cf456bcc1e3aa44b6bba
Hari-Dinesh <dinesh@tericsoft.com>
2024-03-12 11:16:09 +0530

    v1

---
+++ script.js
@@ -0,0 +1,22 @@
+// let name="mr \"dinesh\""
+// console.log(name)
+//functions and the subrountines
+function makecofe(sugar, milk) {
+  var instruction = "Boil the cofee";
+  instruction += "add " + sugar + "spoons of sugar";
+  instruction += "add" + milk + "cups of milk";
+  return instruction;
+}
+makecofe(2, 4);
+// objects and the arrays
+let car = {
+  name: "volvo",
+  model: 4,
+  decs: {
+    speed: 130,
+    modeltypes: ["type1", "type2", { max: "good" }, ["make", "making"]],
+  },
+  ride:function(){return maxSpeedAlter}
+};
+// console.log(car.decs.modeltypes[3][1]);
+//console.log(car.decs);
